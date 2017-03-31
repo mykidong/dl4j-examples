9e2a5ad6641a072dc428fe54c6b8220c4c10da50
Kidong Lee <mykidong@gmail.com>
2017-03-31 20:31:30 +0900

    cnn based mnist example added..

---
+++ dl4j-spark-examples/dl4j-spark/src/main/java/org/deeplearning4j/cnn/MnistExample.java
@@ -0,0 +1,131 @@
+package org.deeplearning4j.cnn;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
+import org.deeplearning4j.spark.impl.paramavg.ParameterAveragingTrainingMaster;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by mykidong on 2017-03-31.
+ */
+public class MnistExample {
+    private static final Logger log = LoggerFactory.getLogger(MnistExample.class);
+
+    public static void main(String[] args) throws Exception {
+        //Create spark context, and load data into memory
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setMaster("local[*]");
+        sparkConf.setAppName("MNIST");
+
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        int examplesPerDataSetObject = 32;
+        DataSetIterator mnistTrain = new MnistDataSetIterator(32, true, 12345);
+        DataSetIterator mnistTest = new MnistDataSetIterator(32, false, 12345);
+        List<DataSet> trainData = new ArrayList<>();
+        List<DataSet> testData = new ArrayList<>();
+        while (mnistTrain.hasNext()) trainData.add(mnistTrain.next());
+        Collections.shuffle(trainData, new Random(12345));
+        while (mnistTest.hasNext()) testData.add(mnistTest.next());
+
+        //Get training data. Note that using parallelize isn't recommended for real problems
+        JavaRDD<DataSet> train = sc.parallelize(trainData);
+        JavaRDD<DataSet> test = sc.parallelize(testData);
+
+        //Set up network configuration (as per standard DL4J networks)
+        int nChannels = 1;
+        int outputNum = 10;
+        int iterations = 1;
+        int seed = 123;
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(seed)
+            .iterations(iterations) // Training iterations as above
+            .regularization(true)
+            .l2(0.0005)
+            .learningRate(.01)//.biasLearningRate(0.02)
+            .weightInit(WeightInit.XAVIER)
+            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+            .updater(Updater.NESTEROVS).momentum(0.9)
+            .list()
+            .layer(0, new ConvolutionLayer.Builder(5, 5)
+                //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters .nIn(nChannels)
+                .stride(1, 1)
+                .nOut(20)
+                .activation(Activation.IDENTITY)
+                .build())
+            .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                .kernelSize(2, 2)
+                .stride(2, 2)
+                .build())
+            .layer(2, new ConvolutionLayer.Builder(5, 5)
+                //Note that nIn need not be specified in later layers
+                .stride(1, 1)
+                .nOut(50)
+                .activation(Activation.IDENTITY)
+                .build())
+            .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                .kernelSize(2, 2)
+                .stride(2, 2)
+                .build())
+            .layer(4, new DenseLayer.Builder().activation(Activation.RELU)
+                .nOut(500).build())
+            .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .nOut(outputNum)
+                .activation(Activation.SOFTMAX)
+                .build())
+            .setInputType(InputType.convolutionalFlat(28, 28, 1)) //See note below
+            .backprop(true).pretrain(false).build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+
+        //Create Spark multi layer network from configuration
+        ParameterAveragingTrainingMaster tm = new ParameterAveragingTrainingMaster.Builder(examplesPerDataSetObject)
+            .workerPrefetchNumBatches(0)
+            .saveUpdater(true)
+            .averagingFrequency(5) //Do 5 minibatch fit operations per
+            .batchSizePerWorker(examplesPerDataSetObject) //Number of examples that each worker
+            .build();
+
+        SparkDl4jMultiLayer sparkNetwork = new SparkDl4jMultiLayer(sc, net, tm);
+        //Train network
+        log.info("--- Starting network training ---");
+        int nEpochs = 5;
+        for (int i = 0; i < nEpochs; i++) {
+            sparkNetwork.fit(train);
+            System.out.println("----- Epoch " + i + " complete -----");
+
+            //Evaluate using Spark:
+            Evaluation evaluation = sparkNetwork.evaluate(test);
+            System.out.println(evaluation.stats());
+        }
+        log.info("****************Example finished********************");
+    }
+
+}
